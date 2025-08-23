b13b684c8c21b740301cc747e2fea4e096528304
pyoh07 <pyoh07@gmail.com>
2025-08-23 14:04:46 +0900

    init study-jpashop

---
+++ src/main/java/jpabook/jpashop/Main.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        // Entity Manager Factory
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        // Entity Manager
+        EntityManager em = emf.createEntityManager();
+        //Transaction 생성
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+
+            //commit
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
