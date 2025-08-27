c235906b5c90bd4ea33d4df0dae26b4940c82665
pyoh07 <pyoh07@gmail.com>
2025-08-27 13:04:02 +0900

    4: 상속관계 매핑

--- src/main/java/jpabook/jpashop/Main.java
+++ src/main/java/jpabook/jpashop/Main.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -18,12 +19,10 @@ public class Main {
         tx.begin();
 
         try{
-            Order order = new Order();
-            em.persist(order);
-
-            OrderItem orderItem = new OrderItem();
-            orderItem.setOrder(order);
-            em.persist(orderItem);
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("aaa");
+            em.persist(book);
 
             //commit
             tx.commit();
